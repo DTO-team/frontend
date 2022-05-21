dcffd5ab25908da508e9ee7154f27bec71971a9c
Bui Gia An <buigiaanfb1@gmail.com>
2022-05-21 21:48:47 +0700

    [An] Config Redux Toolkit and RTK

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Counter } from './features/counter/Counter';
+import Pokemon from './features/pokemon/Pokemon';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>DTO team</h1>
+       <Counter/>
+       <Pokemon/>
       </header>
     </div>
   );
