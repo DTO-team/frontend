78d36a0bdfdd955159824b85cb3701378035d091
megastar24577 <megastar24577@gmail.com>
2022-05-25 09:41:21 +0700

    Fix main page error, add some structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { routes } from 'routes';
 import { useRoutes } from 'react-router-dom';
 
-const App: React.FC = () => {
+const App = () => {
   // The useRoutes() hook allows you to define your routes as JavaScript objects
   // instead of <Routes> and <Route> elements. This is really just a style
   // preference for those who prefer to not use JSX for their routes config.
   let element = useRoutes(routes);
+  return (
     <>
       <h1>DTO team</h1>
       {element}
