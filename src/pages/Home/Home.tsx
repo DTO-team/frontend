0c2877b4e89c733aa7191650113164e46047b413
Bui Gia An <buigiaanfb1@gmail.com>
2022-05-28 00:13:39 +0700

    [NOJIRA] Login-register-flow

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,17 @@
 import Pokemon from 'features/pokemon/Pokemon';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <h1>Home</h1>
       <Pokemon />
+      <div style={{ marginTop: 50 }}>
+        <button onClick={() => navigate('/login')}>Login here</button>
+      </div>
     </>
   );
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,8 @@ import Report from 'pages/Report/Report';
 import Home from 'pages/Home/Home';
 import type { RouteObject } from 'react-router-dom';
 import UserTemplate from 'Templates/UserTemplate';
+import SignInSide from 'Templates/sign-in-side/SignInSide';
+import SignUp from 'Templates/sign-up/SignUp';
 
 export const routes: RouteObject[] = [
   {
@@ -14,5 +16,13 @@ export const routes: RouteObject[] = [
         element: <Report />,
       },
     ],
+  },
+  {
+    path: '/login',
+    element: <SignInSide />,
+  },
+  {
+    path: '/register',
+    element: <SignUp />,
   },
 ];
