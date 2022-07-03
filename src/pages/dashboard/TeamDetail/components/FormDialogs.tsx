34d6fca84dc9c54504397715746f6a980e60456e
Bui Gia An <buigiaanfb1@gmail.com>
2022-07-03 15:36:55 +0700

    [NOJIRA] Update mentor hotfix

--- src/pages/dashboard/TeamDetail/components/FormDialogs.tsx
+++ src/pages/dashboard/TeamDetail/components/FormDialogs.tsx
@@ -27,6 +27,7 @@ import { useSelector } from 'react-redux';
 import { RootState } from 'redux/store';
 import useAuth from 'hooks/useAuth';
 import { AuthorizeRole } from 'utils/enum-utils';
+import { objectsEqual } from 'utils/objectEqual';
 
 // ----------------------------------------------------------------------
 
@@ -48,7 +49,6 @@ export default function FormDialogs({
   inputPlaceholder
 }: FormDialogsProps) {
   const { lecturerList } = useSelector((state: RootState) => state.lecturer);
-  const ref = useRef(0);
   const { user } = useAuth();
 
   const [mentors, setMentors] = useState({
@@ -65,7 +65,10 @@ export default function FormDialogs({
     if (teamDetail && teamDetail.mentors.length > 0) {
       setMentors({
         ...mentors,
-        mentorId: teamDetail.mentors.map((mentor: any) => mentor.id)
+        mentorId: teamDetail.mentors.map((mentor: any) => ({
+          fullName: mentor.fullName,
+          id: mentor.id
+        }))
       });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -76,77 +79,112 @@ export default function FormDialogs({
   };
 
   const handleClose = async () => {
+    setMentors({
+      mentorId: [],
+      newLecturerId: [],
+      projectId: teamDetail.projectId
+    });
     setOpen(false);
   };
 
   const handleChangeMentor = (data: any) => {
-    setMentors({
-      ...mentors,
-      newLecturerId: data.map((mentor: any) => mentor.id)
-    });
+    console.log(data);
+    console.log(mentors);
+    if (!Boolean(data.length)) {
+      setMentors({
+        ...mentors,
+        mentorId: [],
+        newLecturerId: []
+      });
+    } else {
+      const results = data.filter(
+        (mentor: any) => !teamDetail.mentors.some((mentorX: any) => mentorX.id === mentor.id)
+      );
+      setMentors({
+        ...mentors,
+        mentorId: [],
+        newLecturerId: results.map((mentor: any) => mentor.id)
+      });
+    }
   };
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
-    if (ref.current === 0) {
-      ref.current = 1;
-      return;
-    }
-    if (ref.current !== 0) {
-      const { statusCode } = await callAPIForUpdateTeamMentor(mentors);
-      if (statusCode === 200) {
-        enqueueSnackbar('Update successfully!', { variant: 'success' });
-        window.location.reload();
-      } else {
-        enqueueSnackbar('Something went wrong!', { variant: 'error' });
+    if (!Boolean(mentors.mentorId.length) && !Boolean(mentors.newLecturerId.length)) {
+      enqueueSnackbar('Each team must have at least one mentor!', { variant: 'info' });
+    } else {
+      const arraysEqual =
+        teamDetail.mentors.length === mentors.mentorId.length &&
+        teamDetail.mentors.every((o: any, idx: any) =>
+          objectsEqual({ fullName: o.fullName, id: o.id }, mentors.mentorId[idx])
+        );
+      if (arraysEqual) {
+        enqueueSnackbar('Please add or remove!', { variant: 'info' });
+        return;
       }
+      if (Boolean(mentors.newLecturerId.length)) {
+        const { statusCode } = await callAPIForUpdateTeamMentor(mentors, 'add');
+        if (statusCode === 200) {
+          enqueueSnackbar('Update successfully!', { variant: 'success' });
+        } else {
+          enqueueSnackbar('Something went wrong!', { variant: 'error' });
+        }
+      }
+      if (teamDetail.length !== mentors.mentorId.length) {
+        const { statusCode } = await callAPIForUpdateTeamMentor(
+          {
+            ...mentors,
+            newLecturerId: []
+          },
+          'remove'
+        );
+        if (statusCode === 200) {
+          enqueueSnackbar('Update successfully!', { variant: 'success' });
+        } else {
+          enqueueSnackbar('Something went wrong!', { variant: 'error' });
+        }
+      }
+      window.location.reload();
     }
   };
-  return (
-    <form onSubmit={handleSubmit}>
-      {user && user.role === AuthorizeRole.ADMIN && lecturerList && lecturerList.length > 0 && (
-        <>
-          <LoadingButton
-            type="submit"
-            variant="contained"
-            loading={false}
-            onClick={handleClickOpen}
-          >
-            Update mentor
-          </LoadingButton>
-          <Dialog open={open} onClose={handleClose}>
-            <DialogTitle>{title}</DialogTitle>
-            <DialogContent>
-              <DialogContentText>{content}</DialogContentText>
-              <FormControl fullWidth>
-                <Autocomplete
-                  multiple
-                  id="update-mentor"
-                  options={lecturerList}
-                  getOptionLabel={(option) => option.fullName}
-                  defaultValue={teamDetail.mentors.map((lecturer: any) => ({
-                    fullName: lecturer.fullName,
-                    id: lecturer.id
-                  }))}
-                  onChange={(event: any, value) => handleChangeMentor(value)}
-                  filterSelectedOptions
-                  renderInput={(params) => (
-                    <TextField {...params} label="Update mentor" placeholder="Add more mentor" />
-                  )}
-                />
-              </FormControl>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleClose} color="inherit">
-                Cancel
-              </Button>
-              <Button onClick={handleSubmit} variant="contained" type="submit">
-                {id === 'update' ? 'Update' : 'Remove'}
-              </Button>
-            </DialogActions>
-          </Dialog>
-        </>
-      )}
-    </form>
+  return user && user.role === AuthorizeRole.ADMIN && lecturerList && lecturerList.length > 0 ? (
+    <>
+      <LoadingButton type="submit" variant="contained" loading={false} onClick={handleClickOpen}>
+        Update mentor
+      </LoadingButton>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>{title}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>{content}</DialogContentText>
+          <FormControl fullWidth>
+            <Autocomplete
+              multiple
+              id="update-mentor"
+              options={lecturerList}
+              getOptionLabel={(option) => option.fullName}
+              defaultValue={teamDetail.mentors.map((lecturer: any) => ({
+                fullName: lecturer.fullName,
+                id: lecturer.id
+              }))}
+              onChange={(event: any, value) => handleChangeMentor(value)}
+              filterSelectedOptions
+              renderInput={(params) => (
+                <TextField {...params} label="Update mentor" placeholder="Add more mentor" />
+              )}
+            />
+          </FormControl>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="inherit">
+            Cancel
+          </Button>
+          <Button onClick={handleSubmit} variant="contained">
+            {id === 'update' ? 'Update' : 'Remove'}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  ) : (
+    <p>loading</p>
   );
 }
